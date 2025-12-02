29b2b415fb4cfbbf18642f954c29670216368d5b
Capycanth <dillonlehrer1113@gmail.com>
2025-12-01 20:40:19 -0600

    No-Ticket: Fixed all usages of Map

--- srcDB/helpers/helper.ts
+++ srcDB/helpers/helper.ts
@@ -3,20 +3,3 @@ import { ObjectId } from "../model/dataModels";
 export function generateObjectId(): ObjectId {
     return `${Date.now()}-${Math.floor(Math.random() * 1000).toString().padStart(3, '0')}`;
 }
-
-export function safeSerializeMap(map?: Map<unknown, unknown>): string {
-    return map ? JSON.stringify([...map.entries()]) : JSON.stringify([]);
-}
-
-export function safeSerializeNestedMap(nestedMap?: Map<unknown, Map<unknown, unknown>>): string {
-    return nestedMap ? JSON.stringify(
-        [...nestedMap.entries()].map(([key, innerMap]) => [
-            key,
-            [...innerMap.entries()]
-        ])
-    ) : JSON.stringify([]);
-}
-
-export function safeSerializeArray(arr?: unknown[]): string {
-    return arr ? JSON.stringify(arr) : JSON.stringify([]);
-}
